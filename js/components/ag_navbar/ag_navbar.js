ce67febc83339963df7808fe31dff34fade48647
Himanshu Malpande <himanshu.malpande@gmail.com>
2019-08-11 12:18:29 +0530

    Improved AgNavbar

--- js/components/ag_navbar/ag_navbar.js
+++ js/components/ag_navbar/ag_navbar.js
@@ -3,9 +3,9 @@ class AgNavbar extends Component {
 	selector = "ag-navbar";
 	styleSheets = ["/js/components/ag_navbar/ag_navbar.css"];
 	onInit() {
-		var navbar = this.element.find(".ag-navbar")[0];
-		for (var i = 0; i < this.$children.length; i++) {
-			navbar.nativeElement.appendChild(this.$children[i]);
+		this.navbar = this.element.find(".ag-navbar")[0];
+		for (var e of this.$children) {
+			this.navbar.nativeElement.appendChild(e);
 		}
 	}
 }
